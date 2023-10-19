68e3e257704d44dcc5a04ae6758993d59aad6ecd
KenLin765 <98061901+KenLin765@users.noreply.github.com>
2023-10-19 23:04:56 +0100

    uploaded lab 5 final

--- labs/lab05.03-githubbymodule.py
+++ labs/lab05.03-githubbymodule.py
@@ -1,5 +1,6 @@
 from github import Github
 from config import githubkey
+import requests
 
 github_username = 'KenLin765'
 repository_name = 'private'
@@ -12,4 +13,19 @@ repo = g.get_repo(f"{github_username}/{repository_name}")
 clone_url = repo.clone_url
 
 print(f"Clone URL for {repository_name}: {clone_url}")
+
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+#print (urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+print (contentOfFile)
+
+newContents = contentOfFile + " more stuff \n"
+
+gitHubResponse=repo.update_file(fileInfo.path,"updated by prog",
+newContents,fileInfo.sha)
+print (gitHubResponse)
+
 
